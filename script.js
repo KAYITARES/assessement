71266bc074af3f68916638aa00c233b6fc1879fb
kayitare cynthia <cyntkayitare@gmail.com>
2018-12-14 11:52:00 +0200

    adding name of triagle in scrit.js

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 function outputmessage() {
-    var firstNumber= document.getElementById("firstvalue").value;
-    var secondNumber=document.getElementById("secondtvalue").value;
-    var thirdNumber= document.getElementById("thirdvalue").value;
+    var firstNumber= parseInt(document.getElementById("firstvalue").value);
+    var secondNumber=parseInt(document.getElementById("secondtvalue").value);
+    var thirdNumber= parseInt(document.getElementById("thirdvalue").value);
     var text;
 
      if(firstNumber <=0 || secondNumber<=0 || thirdNumber<=0){
          text="A triangle must have 3 sides with positive definate length!";
+     }
+        else if (
+            (firstNumber + secondNumber <= thirdNumber &&
+                secondNumber + firstNumber <= thirdNumber) ||
+            (firstNumber + thirdNumber <= secondNumber &&
+                thirdNumber + firstNumber <=secondNumber ) ||
+            (secondNumber + thirdNumber <= firstNumber && thirdNumber + secondNumber <= firstNumber)
+          ) {  
+            text="Not a Triangle";
+            
     }
-    //if is not trianle
-    else if(firstNumber + secondNumber <= thirdNumber && firstNumber + thirdNumber <= secondNumber && secondNumber + thirdNumber <= firstNumber){
-        text = "Is Not Triangle";
-    
-    }
+
     //if triangle is equilateral
-    else if(firstNumber===secondNumber && secondNumber===thirdNumber) {
+    else if(
+        firstNumber===secondNumber && 
+        secondNumber===thirdNumber &&
+        thirdNumber===firstNumber
+     ) {
         text = "The triangle is equilateral.";   
     }
+    
     //if triangle is isosceles
-    else if(firstNumber===secondNumber || firstNumber===thirdNumber || secondNumber===thirdNumber) {
+    else if(
+        firstNumber===secondNumber || 
+        secondNumber===thirdNumber || 
+        thirdNumber===firstNumber
+        ) {
         text="The triangle is isosceles.";
     } 
     //if triangle is scalene
-    else if(firstNumber!==secondNumber && firstNumber!==thirdNumber && secondNumber!==thirdNumber) {
-        text="triangle is scalene ";
-    }
-    
+    else if (
+        firstNumber!==secondNumber &&
+           
+        firstNumber!==thirdNumber &&
+            
+        secondNumber!== thirdNumber 
+      ) {  
+        text="Triangle is scalene";     
+}
     else {
            text = "error";
         }
